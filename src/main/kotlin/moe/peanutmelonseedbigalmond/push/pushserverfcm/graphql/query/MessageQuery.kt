07aca472815f2550468ded38428307dc68e1487f
13574 <1357496606@qq.com>
2023-11-01 16:40:27 +0800

    feat: 按 id 查询消息内容

---
+++ src/main/kotlin/moe/peanutmelonseedbigalmond/push/pushserverfcm/graphql/query/MessageQuery.kt
@@ -0,0 +1,30 @@
+package moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.query
+
+import graphql.kickstart.tools.GraphQLQueryResolver
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.db.repository.LoginTokenWrapper
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.db.repository.MessageRepository
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.GraphqlException
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.bean.MessagesQLBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.NotBlank
+
+@Component
+@Validated
+class MessageQuery : GraphQLQueryResolver {
+    @Autowired
+    private lateinit var loginTokenQuery: LoginTokenWrapper
+
+    @Autowired
+    private lateinit var messageRepository: MessageRepository
+
+    fun queryMessage(@NotBlank token: String, id: Long): MessagesQLBean {
+        val uid = loginTokenQuery.getLoginTokenInfoByToken(token).belongsTo
+        val message = messageRepository.findByMessageIdAndOwnerAndNotDeleted(uid, id)
+            ?: throw GraphqlException("message does not exists")
+        return message.let {
+            return@let MessagesQLBean(it.owner, it.messageId, it.type, it.text, it.title, it.pushTime, it.topicId)
+        }
+    }
+}
