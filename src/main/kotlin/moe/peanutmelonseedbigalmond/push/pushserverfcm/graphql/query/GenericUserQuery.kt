ee69cc4d8ee1052e1e927413ecba7d1c226220fa
PeanutMelonSeedBigAlmond <PeanutMelonSeedBigAlmond@outlook.com>
2024-01-01 13:34:39 +0800

    feat: 修改登录接口

--- src/main/kotlin/moe/peanutmelonseedbigalmond/push/pushserverfcm/graphql/query/GenericUserQuery.kt
+++ src/main/kotlin/moe/peanutmelonseedbigalmond/push/pushserverfcm/graphql/query/GenericUserQuery.kt
@@ -2,7 +2,8 @@ package moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.query
 
 import graphql.kickstart.tools.GraphQLQueryResolver
 import moe.peanutmelonseedbigalmond.push.pushserverfcm.db.repository.LoginTokenWrapper
-import moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.bean.TokenQLBean
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.db.repository.UserRepository
+import moe.peanutmelonseedbigalmond.push.pushserverfcm.graphql.bean.UserQLBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.validation.annotation.Validated
@@ -14,9 +15,13 @@ class GenericUserQuery : GraphQLQueryResolver {
     @Autowired
     private lateinit var loginTokenRepository: LoginTokenWrapper
 
-    fun token(@NotBlank loginToken: String): TokenQLBean? {
-        val token = loginTokenRepository.getLoginTokenInfoByToken(loginToken)
+    @Autowired
+    private lateinit var userRepository: UserRepository
 
-        return TokenQLBean(token.belongsTo, token.token, token.expiredAt)
+    fun getUser(@NotBlank token: String): UserQLBean {
+        val tokenInfo = loginTokenRepository.getLoginTokenInfoByToken(token)
+        val userInfo = userRepository.getUserInfoByUid(tokenInfo.id)!!
+
+        return UserQLBean(userInfo.username, userInfo.uid)
     }
 }
